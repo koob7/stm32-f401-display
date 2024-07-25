8e7f543355f1f8e968f7b368080971758aae3049
kobie <kobielus63@gmail.com>
2024-07-25 23:43:42 +0200

    prosta obsługa silników krkowych

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -159,99 +159,103 @@ int main(void)
 
   uint16_t pos_y;
     TFT_Clear_Screen(0xCFFF);
-    TFT_Draw_Fill_Rectangle(100,0, 600, 144, 0xD61F);//niebieska ramka
+//    TFT_Draw_Fill_Rectangle(100,0, 600, 144, 0xD61F);//niebieska ramka
+//
+//
+//
+//
+//	TFT_Draw_Fill_Rectangle(780,0, 20, 20, 0x8C51);//szary prostokąt z prawej
+//	TFT_Draw_Fill_Rectangle(780,460, 20, 20, 0x8C51);
+//	TFT_Draw_Fill_Round_Rect (680, 0, 120, 480, 20, 0x8C51);
+//	pos_y = 9;
+//	for(uint8_t i = 0; i<7;i+=1)//przyciski po prawej
+//	{
+//		TFT_Draw_Fill_Round_Rect (696, pos_y, 88, 47, 10,  0xD6BA);
+//		TFT_Draw_Fill_Rectangle(740,pos_y, 2, 47, 0x0000);
+//		LCD_centered_Font(696, pos_y+24, 88, "+   -", _Open_Sans_Bold_48, 1, 0x4A69);
+//		pos_y+=56;
+//	}
+//	TFT_Draw_Fill_Round_Rect (696, 413, 88, 47, 10,  0xD6BA);
+//	TFT_Draw_Fill_Rectangle(740, 413, 2, 47, 0x0000);
+//	LCD_centered_Font(696, 413+24, 88, "+   -", _Open_Sans_Bold_48, 1, 0x4A69);
+//	LCD_centered_Font(696, 9+25, 88, "X", _Open_Sans_Bold_48, 1, 0x7D3D);//literki po prawej
+//	LCD_centered_Font(696, 65+25, 88, "Y", _Open_Sans_Bold_48, 1, 0x7D3D);
+//	LCD_centered_Font(696, 121+25, 88, "Z", _Open_Sans_Bold_48, 1, 0x7D3D);
+//	LCD_centered_Font(696, 177+25, 88, "A", _Open_Sans_Bold_48, 1, 0x7D3D);
+//	LCD_centered_Font(696, 233+25, 88, "B", _Open_Sans_Bold_48, 1, 0x7D3D);
+//	LCD_centered_Font(696, 289+25, 88, "C", _Open_Sans_Bold_48, 1, 0x7D3D);
+//	LCD_FillTriangle(732, 417,732, 417+38, 732+24, 417+19, 0x7D3D);
+//	TFT_Draw_Bitmap_Without_Background(721, 353, hand_icon_x, hand_icon_y, hand_icon);
+//
+//
+//
+//
+//    TFT_Draw_Fill_Rectangle(0,0, 20, 20, 0x8C51);//szary prostokąt z lewej
+//    TFT_Draw_Fill_Rectangle(0,460, 20, 20, 0x8C51);
+//    TFT_Draw_Fill_Round_Rect (0, 0, 120, 480, 20, 0x8C51);
+//    pos_y = 33;
+//    for(uint8_t i = 0; i<8;i+=1)//przyciski po lewej
+//    {
+//        TFT_Draw_Fill_Round_Rect (16, pos_y, 88, 47, 10,  0xD6BA);
+//        pos_y+=56;
+//    }
+//    LCD_centered_Font(0, 0+16, 120, "JOB NAME", _Open_Sans_Bold_14, 1, BLACK);
+//	LCD_centered_Font(16, 33+24, 88, "OPEN", _Open_Sans_Bold_14, 1, 0x7D3D);//literki po lewej
+//	LCD_centered_Font(16, 89+24, 88, "CONT. JOB", _Open_Sans_Bold_14, 1, 0x7D3D);
+//	LCD_centered_Font(16, 145+24, 88, "MANUAL", _Open_Sans_Bold_14, 1, 0x7D3D);
+//	LCD_centered_Font(16, 201+14, 88, "ENABLE\nTOOL", _Open_Sans_Bold_14, 1, 0x7D3D);
+//	LCD_centered_Font(16, 257+24, 88, "RUN JOB", _Open_Sans_Bold_14, 1, 0x7D3D);
+//	LCD_centered_Font(16, 313+14, 88, "AXIS\nCONTROL", _Open_Sans_Bold_14, 1, 0x7D3D);
+//	LCD_centered_Font(16, 369+14, 88, "CONT.\nMOVEMENT", _Open_Sans_Bold_14, 1, 0x7D3D);
+//	LCD_centered_Font(16, 425+24, 88, "SETTINGS", _Open_Sans_Bold_14, 1, 0x7D3D);
+//
+//
+//
+//
+//	TFT_Draw_Fill_Round_Rect (132, 9, 70, 41, 20,  0xD6BA);//przyciski na gorze
+//	TFT_Draw_Fill_Round_Rect (212, 9, 70, 41, 20,  0xD6BA);
+//	TFT_Draw_Fill_Round_Rect (291, 9, 70, 41, 20,  0xD6BA);
+//	TFT_Draw_Fill_Round_Rect (371, 9, 81, 41, 20,  0xD6BA);
+//	TFT_Draw_Fill_Round_Rect (462, 9, 72, 41, 20,  0xD6BA);
+//	TFT_Draw_Fill_Round_Rect (544, 9, 72, 41, 20,  0xD6BA);
+//	TFT_Draw_Fill_Round_Rect (628, 15, 40, 40, 20,  0xD6BA);
+//	TFT_Draw_Fill_Round_Rect (628, 88, 40, 40, 20,  0xD6BA);
+//	TFT_Draw_Fill_Round_Rect (211, 68, 61, 21, 10,  0xD6BA);
+//	TFT_Draw_Fill_Round_Rect (211, 109, 61, 21, 10,  0xD6BA);
+//	TFT_Draw_Fill_Round_Rect (362, 68, 61, 21, 10,  0xD6BA);
+//	TFT_Draw_Fill_Round_Rect (362, 109, 61, 21, 10,  0xD6BA);
+//	TFT_Draw_Fill_Round_Rect (532, 68, 61, 21, 10,  0xD6BA);
+//	TFT_Draw_Fill_Round_Rect (532, 109, 61, 21, 10,  0xD6BA);
+//    LCD_centered_Font(132, 9+22, 70, "SELECT", _Open_Sans_Bold_14, 1, BLACK);//opisy przyciskow
+//    LCD_centered_Font(212, 9+22, 70, "NEW P.", _Open_Sans_Bold_14, 1, BLACK);
+//    LCD_centered_Font(291, 9+22, 70, "EDIT P.", _Open_Sans_Bold_14, 1, BLACK);
+//    LCD_Font(371, 9+22,  "DELETE P.", _Open_Sans_Bold_14, 1, BLACK);
+//    LCD_centered_Font(462, 9+22, 70, "SET T.P.", _Open_Sans_Bold_14, 1, BLACK);
+//    LCD_centered_Font(544, 9+22, 70, "SET H.P", _Open_Sans_Bold_14, 1, BLACK);
+//    LCD_Font(155, 67+11, "Name:", _Open_Sans_Bold_14, 1, BLACK);
+//    LCD_Font(155, 109+11, "Tool:", _Open_Sans_Bold_14, 1, BLACK);
+//    LCD_Font(295, 67+11, "Move:", _Open_Sans_Bold_14, 1, BLACK);
+//    LCD_Font(295, 109+11, "Speed:", _Open_Sans_Bold_14, 1, BLACK);
+//    LCD_Font(446, 67+11, "Type:", _Open_Sans_Bold_14, 1, BLACK);
+//    LCD_Font(446, 109+11, "at Target:", _Open_Sans_Bold_14, 1, BLACK);
+//    LCD_centered_Font(211, 68+11, 61, "P11", _Open_Sans_Bold_14, 1, BLACK);
+//    LCD_centered_Font(211, 109+11, 41, "OFF", _Open_Sans_Bold_14, 1, BLACK);
+//    LCD_centered_Font(362, 68+11, 41, "Circ", _Open_Sans_Bold_14, 1, BLACK);
+//    LCD_centered_Font(362, 109+11, 41, "100%", _Open_Sans_Bold_14, 1, BLACK);
+//    LCD_centered_Font(532, 68+11, 41, "Cont", _Open_Sans_Bold_14, 1, BLACK);
+//    LCD_centered_Font(532, 109+11, 61, "YES", _Open_Sans_Bold_14, 1, BLACK);
+//	LCD_FillTriangle(636, 42,636+23, 42, 647, 22, 0x00FD);
+//	LCD_FillTriangle(636, 42+59,636+23, 42+59, 647, 22+40+59, 0x00FD);
+//	LCD_FillTriangle(574, 74,573+13, 74, 580, 74+11, 0x00FD);
+//	//LCD_FillTriangle(574, 74+41,573+13, 74+41, 580, 74+11+41, 0x00FD);
+//	LCD_FillTriangle(574-170, 74,573+13-170, 74, 580-170, 74+11, 0x00FD);
+//	LCD_FillTriangle(574-170, 74+41,573+13-170, 74+41, 580-170, 74+11+41, 0x00FD);
+//	LCD_FillTriangle(574-322, 74+41,573+13-322, 74+41, 580-322, 74+11+41, 0x00FD);
 
-
-
-
-	TFT_Draw_Fill_Rectangle(780,0, 20, 20, 0x8C51);//szary prostokąt z prawej
-	TFT_Draw_Fill_Rectangle(780,460, 20, 20, 0x8C51);
-	TFT_Draw_Fill_Round_Rect (680, 0, 120, 480, 20, 0x8C51);
-	pos_y = 9;
-	for(uint8_t i = 0; i<7;i+=1)//przyciski po prawej
-	{
-		TFT_Draw_Fill_Round_Rect (696, pos_y, 88, 47, 10,  0xD6BA);
-		TFT_Draw_Fill_Rectangle(740,pos_y, 2, 47, 0x0000);
-		LCD_centered_Font(696, pos_y+24, 88, "+   -", _Open_Sans_Bold_48, 1, 0x4A69);
-		pos_y+=56;
-	}
-	TFT_Draw_Fill_Round_Rect (696, 413, 88, 47, 10,  0xD6BA);
-	TFT_Draw_Fill_Rectangle(740, 413, 2, 47, 0x0000);
-	LCD_centered_Font(696, 413+24, 88, "+   -", _Open_Sans_Bold_48, 1, 0x4A69);
-	LCD_centered_Font(696, 9+25, 88, "X", _Open_Sans_Bold_48, 1, 0x7D3D);//literki po prawej
-	LCD_centered_Font(696, 65+25, 88, "Y", _Open_Sans_Bold_48, 1, 0x7D3D);
-	LCD_centered_Font(696, 121+25, 88, "Z", _Open_Sans_Bold_48, 1, 0x7D3D);
-	LCD_centered_Font(696, 177+25, 88, "A", _Open_Sans_Bold_48, 1, 0x7D3D);
-	LCD_centered_Font(696, 233+25, 88, "B", _Open_Sans_Bold_48, 1, 0x7D3D);
-	LCD_centered_Font(696, 289+25, 88, "C", _Open_Sans_Bold_48, 1, 0x7D3D);
-	LCD_FillTriangle(732, 417,732, 417+38, 732+24, 417+19, 0x7D3D);
-	TFT_Draw_Bitmap_Without_Background(721, 353, hand_icon_x, hand_icon_y, hand_icon);
-
-
-
-
-    TFT_Draw_Fill_Rectangle(0,0, 20, 20, 0x8C51);//szary prostokąt z lewej
-    TFT_Draw_Fill_Rectangle(0,460, 20, 20, 0x8C51);
-    TFT_Draw_Fill_Round_Rect (0, 0, 120, 480, 20, 0x8C51);
-    pos_y = 33;
-    for(uint8_t i = 0; i<8;i+=1)//przyciski po lewej
-    {
-        TFT_Draw_Fill_Round_Rect (16, pos_y, 88, 47, 10,  0xD6BA);
-        pos_y+=56;
-    }
-    LCD_centered_Font(0, 0+16, 120, "JOB NAME", _Open_Sans_Bold_14, 1, BLACK);
-	LCD_centered_Font(16, 33+24, 88, "OPEN", _Open_Sans_Bold_14, 1, 0x7D3D);//literki po lewej
-	LCD_centered_Font(16, 89+24, 88, "CONT. JOB", _Open_Sans_Bold_14, 1, 0x7D3D);
-	LCD_centered_Font(16, 145+24, 88, "MANUAL", _Open_Sans_Bold_14, 1, 0x7D3D);
-	LCD_centered_Font(16, 201+14, 88, "ENABLE\nTOOL", _Open_Sans_Bold_14, 1, 0x7D3D);
-	LCD_centered_Font(16, 257+24, 88, "RUN JOB", _Open_Sans_Bold_14, 1, 0x7D3D);
-	LCD_centered_Font(16, 313+14, 88, "AXIS\nCONTROL", _Open_Sans_Bold_14, 1, 0x7D3D);
-	LCD_centered_Font(16, 369+14, 88, "CONT.\nMOVEMENT", _Open_Sans_Bold_14, 1, 0x7D3D);
-	LCD_centered_Font(16, 425+24, 88, "SETTINGS", _Open_Sans_Bold_14, 1, 0x7D3D);
-
-
-
-
-	TFT_Draw_Fill_Round_Rect (132, 9, 70, 41, 20,  0xD6BA);//przyciski na gorze
-	TFT_Draw_Fill_Round_Rect (212, 9, 70, 41, 20,  0xD6BA);
-	TFT_Draw_Fill_Round_Rect (291, 9, 70, 41, 20,  0xD6BA);
-	TFT_Draw_Fill_Round_Rect (371, 9, 81, 41, 20,  0xD6BA);
-	TFT_Draw_Fill_Round_Rect (462, 9, 72, 41, 20,  0xD6BA);
-	TFT_Draw_Fill_Round_Rect (544, 9, 72, 41, 20,  0xD6BA);
-	TFT_Draw_Fill_Round_Rect (628, 15, 40, 40, 20,  0xD6BA);
-	TFT_Draw_Fill_Round_Rect (628, 88, 40, 40, 20,  0xD6BA);
-	TFT_Draw_Fill_Round_Rect (211, 68, 61, 21, 10,  0xD6BA);
-	TFT_Draw_Fill_Round_Rect (211, 109, 61, 21, 10,  0xD6BA);
-	TFT_Draw_Fill_Round_Rect (362, 68, 61, 21, 10,  0xD6BA);
-	TFT_Draw_Fill_Round_Rect (362, 109, 61, 21, 10,  0xD6BA);
-	TFT_Draw_Fill_Round_Rect (532, 68, 61, 21, 10,  0xD6BA);
-	TFT_Draw_Fill_Round_Rect (532, 109, 61, 21, 10,  0xD6BA);
-    LCD_centered_Font(132, 9+22, 70, "SELECT", _Open_Sans_Bold_14, 1, BLACK);//opisy przyciskow
-    LCD_centered_Font(212, 9+22, 70, "NEW P.", _Open_Sans_Bold_14, 1, BLACK);
-    LCD_centered_Font(291, 9+22, 70, "EDIT P.", _Open_Sans_Bold_14, 1, BLACK);
-    LCD_Font(371, 9+22,  "DELETE P.", _Open_Sans_Bold_14, 1, BLACK);
-    LCD_centered_Font(462, 9+22, 70, "SET T.P.", _Open_Sans_Bold_14, 1, BLACK);
-    LCD_centered_Font(544, 9+22, 70, "SET H.P", _Open_Sans_Bold_14, 1, BLACK);
-    LCD_Font(155, 67+11, "Name:", _Open_Sans_Bold_14, 1, BLACK);
-    LCD_Font(155, 109+11, "Tool:", _Open_Sans_Bold_14, 1, BLACK);
-    LCD_Font(295, 67+11, "Move:", _Open_Sans_Bold_14, 1, BLACK);
-    LCD_Font(295, 109+11, "Speed:", _Open_Sans_Bold_14, 1, BLACK);
-    LCD_Font(446, 67+11, "Type:", _Open_Sans_Bold_14, 1, BLACK);
-    LCD_Font(446, 109+11, "at Target:", _Open_Sans_Bold_14, 1, BLACK);
-    LCD_centered_Font(211, 68+11, 61, "P11", _Open_Sans_Bold_14, 1, BLACK);
-    LCD_centered_Font(211, 109+11, 41, "OFF", _Open_Sans_Bold_14, 1, BLACK);
-    LCD_centered_Font(362, 68+11, 41, "Circ", _Open_Sans_Bold_14, 1, BLACK);
-    LCD_centered_Font(362, 109+11, 41, "100%", _Open_Sans_Bold_14, 1, BLACK);
-    LCD_centered_Font(532, 68+11, 41, "Cont", _Open_Sans_Bold_14, 1, BLACK);
-    LCD_centered_Font(532, 109+11, 61, "YES", _Open_Sans_Bold_14, 1, BLACK);
-	LCD_FillTriangle(636, 42,636+23, 42, 647, 22, 0x00FD);
-	LCD_FillTriangle(636, 42+59,636+23, 42+59, 647, 22+40+59, 0x00FD);
-	LCD_FillTriangle(574, 74,573+13, 74, 580, 74+11, 0x00FD);
-	//LCD_FillTriangle(574, 74+41,573+13, 74+41, 580, 74+11+41, 0x00FD);
-	LCD_FillTriangle(574-170, 74,573+13-170, 74, 580-170, 74+11, 0x00FD);
-	LCD_FillTriangle(574-170, 74+41,573+13-170, 74+41, 580-170, 74+11+41, 0x00FD);
-	LCD_FillTriangle(574-322, 74+41,573+13-322, 74+41, 580-322, 74+11+41, 0x00FD);
-
+    TFT_Draw_Fill_Round_Rect (100, 180, 200, 120, 20,  0xD6BA);
+    LCD_centered_Font(100, 240, 200, "Prawo", _Open_Sans_Bold_28, 1, BLACK);
+    TFT_Draw_Fill_Round_Rect (500, 180, 200, 120, 20,  0xD6BA);
+    LCD_centered_Font(500, 240, 200, "Lewo", _Open_Sans_Bold_28, 1, BLACK);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -272,34 +276,55 @@ int main(void)
 		  was_touched=0;
 		NVIC_DisableIRQ(EXTI15_10_IRQn);
 		uint16_t touchx, touchy;
-		char buffer1[10]=""; // Bufor na konwertowaną wartość
-		char buffer2[10]=""; // Bufor na konwertowaną wartość
-		TFT_Draw_Fill_Round_Rect (280, 180, 200, 60, 10,  0xCFFF);
+//		char buffer1[10]=""; // Bufor na konwertowaną wartość
+//		char buffer2[10]=""; // Bufor na konwertowaną wartość
+//		TFT_Draw_Fill_Round_Rect (280, 180, 200, 60, 10,  0xCFFF);
 		touchx = getX();
-		sprintf(buffer1, "X%d", touchx); // Konwersja wartości do ciągu znaków
+//		sprintf(buffer1, "X%d", touchx); // Konwersja wartości do ciągu znaków
 		touchy = getY();
-		sprintf(buffer2, "Y%d", touchy); // Konwersja wartości do ciągu znaków
+//		sprintf(buffer2, "Y%d", touchy); // Konwersja wartości do ciągu znaków
 
-		LCD_Font(300, 200,  buffer1, _Open_Sans_Bold_28, 1, BLACK);
-		LCD_Font(300, 220,  buffer2, _Open_Sans_Bold_28, 1, BLACK);
-		HAL_Delay(100);
+//		LCD_Font(300, 200,  buffer1, _Open_Sans_Bold_28, 1, BLACK);
+//		LCD_Font(300, 220,  buffer2, _Open_Sans_Bold_28, 1, BLACK);
+//		HAL_Delay(100);
 
 //		for (int i =0; i<10;i+=1)
 //		{
 //			asm("nop");
 //		}
-		XPT2046_Init();
 
 
-		if(touchx >=696 && touchx<=696+88 && touchy>=9 && touchy<=9+47)// 696, pos_y, 88, 47,
-			 		  		{
-			  	  	  	  	  	  	uint16_t counter = TFT_Draw_List(400, 200, 100, "TYPE:", "powitanie",  save, _Open_Sans_Bold_14);
-			  	  	  	  	  	  	HAL_Delay(3000);
-			  	  	  	  	  	  	TFT_Restore_Area(400, 200, 100, 47+1+34+35*counter, save);
-			 		  		}
-	  }
-		__HAL_GPIO_EXTI_CLEAR_IT(T_IRQ_Pin);//czyszczenie zgłoszonego przerwania
-		NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+	if(touchx >=100 && touchx<=300 && touchy>=180 && touchy<=300)// 696, pos_y, 88, 47,
+	{
+		HAL_GPIO_WritePin(dir_GPIO_Port , dir_Pin, GPIO_PIN_SET);
+		was_touched=1;
+		for(int i =0; i<10; i++){
+			  HAL_GPIO_WritePin(step_GPIO_Port , step_Pin, GPIO_PIN_SET);
+			  HAL_GPIO_WritePin(step_GPIO_Port , step_Pin, GPIO_PIN_RESET);
+			  for(int j =0; j<20000; j++){
+				  asm("NOP");
+			  }
+		  }
+	}
+	if(touchx >=500 && touchx<=700 && touchy>=180 && touchy<=300)// 696, pos_y, 88, 47,
+	{
+		HAL_GPIO_WritePin(dir_GPIO_Port , dir_Pin, GPIO_PIN_RESET);
+		was_touched=1;
+		for(int i =0; i<10; i++){
+			  HAL_GPIO_WritePin(step_GPIO_Port , step_Pin, GPIO_PIN_SET);
+			  HAL_GPIO_WritePin(step_GPIO_Port , step_Pin, GPIO_PIN_RESET);
+			  for(int j =0; j<20000; j++){
+				  asm("NOP");
+			  }
+		  }
+
+	}
+
+	XPT2046_Init();
+  }
+	__HAL_GPIO_EXTI_CLEAR_IT(T_IRQ_Pin);//czyszczenie zgłoszonego przerwania
+	NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 //		HAL_Delay(1000);
 //		uint16_t counter3 = TFT_Draw_List(400, 200, 100, "TYPE:", "placek ciastko lody lizak",  save, _Open_Sans_Bold_14);
